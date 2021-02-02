5d97d189810b185d0f075c1817ed5a7a54e0786e
Julian Panucci <julian.panucci@underarmour.com>
2021-02-01 18:46:36 -0600

    Stuff

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MyLib",
+	platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLib",
-            dependencies: []),
+            dependencies: ["colorChanger"]),
         .testTarget(
             name: "MyLibTests",
             dependencies: ["MyLib"]),
--- Sources/MyLib/MyLib.swift
+++ Sources/MyLib/MyLib.swift
@@ -1,3 +1,6 @@
+import colorChanger
+
 public struct MyLib {
     public var text = "Hello, World!"
+	
 }
